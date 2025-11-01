3371498518ed1f5e56c18eb54ed26d04a1eb5953
RAndrea976 <landew976@gmail.com>
2025-11-01 21:30:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const outMinMax = getElementById("minMax");
 const outVett = getElementById("Vett");
 const outVett50 = getElementById("Vett50");
+let min = 0;
+let max = 0;
 
 let v = []
 for(let i = 0; i < 100; i++) {
-  v[i] = Math.floor(Mat.random() * 200) - 100;
+  v[i] = Math.floor(Math.random() * 200) - 100;
+  if(i == 0) {
+    min = v[0];
+    max = v[0];
+  } else if(max < v[i]) {
+    max = v[i];
+  } else if(min > v[i) {
+    min = v[i];
+  }
 }
 
+let v50 = [];
+let c = 0;
+for(let i = 0; i < 100; i++) {
+  if(v[i] > 50) {
+    v50[c] = v[i];
+    c++;
+  }
+}
