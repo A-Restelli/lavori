74a74f861a9ea08672468910d6eeb4caa803f024
RAndrea976 <landew976@gmail.com>
2025-11-01 20:32:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const outMinMax = getElementById("minMax");
+const outVett = getElementById("Vett");
+const outVett50 = getElementById("Vett50");
+
+let v = []
+for(let i = 0; i < 100; i++) {
+  v[i] = Math.floor(Mat.random() * 200) - 100;
+}
 
